f3a05f9ceb6a99c3631fa6cf62f4c10f7a9624c4
yghatti <yash.ghatti@ehealthsask.ca>
2022-12-27 21:16:30 -0600

    Initial Commit

---
+++ src/main/java/ca/ylo/ws/YloWebServiceApplication.java
@@ -0,0 +1,13 @@
+package ca.ylo.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YloWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YloWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/ylo/ws/YloWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ylo.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YloWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
